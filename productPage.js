40d9214c2ea83b32fa25c320b62ad51fb256270e
eli2376 <elimershine@gmail.com>
2023-08-08 16:06:22 +0300

    first commit

---
+++ productPage.js
@@ -0,0 +1,6 @@
+const body = document.createElement("body")
+const header = document.createElement("header")
+header.textContent = "Inventory Managmant"
+header.appendChild(<i class="fa-solid fa-house"></i>)
+body.appendChild(header)
+
